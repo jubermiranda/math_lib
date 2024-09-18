c8375308da224e00130faa0b47e2f9d0f4a54921
uniapp <uniapp@uniapp>
2024-09-18 09:34:09 -0400

    clear crr tests. add new test for null mtr class

--- tests/test_matrix_lib.cxx
+++ tests/test_matrix_lib.cxx
@@ -8,34 +8,32 @@ using namespace std;
 class MatrixLibFixture : public ::testing::Test {
 protected:
   void SetUp() override {
-    this->test_mtr = new Matrix(lines, columns);
-    vector<int> mtr_elements = {
+  }
+
+  void TearDown() override {
+  }
+
+};
+
+
+TEST_F(MatrixLibFixture, CreateGetLineColumns) {
+  Matrix test_mtr = Matrix(4,2);
+
+  EXPECT_EQ(test_mtr.get_lines(), 4);
+  EXPECT_EQ(test_mtr.get_columns(), 2);
+}
+
+TEST_F(MatrixLibFixture, PrintMtrElements) {
+  Matrix test_mtr = Matrix(4,4);
+  vector<int> elements = {
       1,0,0,0,
       0,1,0,0,
       0,0,1,0,
       0,0,0,1
-    };
-    this->test_mtr->set_elements(mtr_elements);
-  }
+  };
+  test_mtr.set_elements(elements);
 
-  void TearDown() override {
-    delete this->test_mtr;
-  }
-
-  unsigned lines = 4;
-  unsigned columns = 4;
-  Matrix *test_mtr;
-};
-
-
-TEST_F(MatrixLibFixture, CreatedMtrTest) {
-  ASSERT_TRUE(test_mtr != nullptr);
-  EXPECT_EQ(lines, test_mtr->get_lines());
-  EXPECT_EQ(columns, test_mtr->get_columns());
-}
-
-TEST_F(MatrixLibFixture, PrintMtrElements) {
-  string mtr_string = this->test_mtr->print().str();
+  string mtr_string = test_mtr.print().str();
 
   unsigned expected_0_ocrr = 12;
   unsigned expected_1_ocrr = 4;
@@ -49,8 +47,7 @@ TEST_F(MatrixLibFixture, PrintMtrElements) {
 
 
 TEST_F(MatrixLibFixture, MtrClassIdentity) {
-  string lhs = "identity";
-  string rhs;
+  string result_class;
   vector<int> mtr_elements;
 
   Matrix test_mtr = Matrix(4,4);
@@ -61,11 +58,13 @@ TEST_F(MatrixLibFixture, MtrClassIdentity) {
       0,0,0,1
   };
   test_mtr.set_elements(mtr_elements);
+  result_class = test_mtr.print_class().str();
 
-  rhs = test_mtr.print_class().str();
-  EXPECT_TRUE(t_utils::is_sub_str(rhs, lhs));
+  EXPECT_TRUE(t_utils::is_sub_str(result_class, "identity"));
+  EXPECT_TRUE(t_utils::is_sub_str(result_class, "square"));
 
   mtr_elements.clear();
+
 
   Matrix test_mtr2 = Matrix(2,2);
   mtr_elements = {
@@ -73,7 +72,13 @@ TEST_F(MatrixLibFixture, MtrClassIdentity) {
     0,1
   };
   test_mtr2.set_elements(mtr_elements);
+  result_class = test_mtr2.print_class().str();
 
-  rhs = test_mtr2.print_class().str();
-  EXPECT_TRUE(t_utils::is_sub_str(rhs, lhs));
+  EXPECT_TRUE(t_utils::is_sub_str(result_class, "identity"));
+  EXPECT_TRUE(t_utils::is_sub_str(result_class, "square"));
+}
+
+
+TEST_F(MatrixLibFixture, MtrClassNull) {
+  ASSERT_EQ(4,2);
 }
