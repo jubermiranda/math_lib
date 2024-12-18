7173f1f34678e81620d337a902241d08935f6ae5
uniapp <uniapp@uniapp>
2024-12-18 09:54:15 -0500

    initial tests [to_...]

--- include/rational.h
+++ include/rational.h
@@ -1,13 +1,30 @@
 #ifndef RATIONAL_H_
 #define RATIONAL_H_
 
+#include <string>
+
 class Rational {
 public:
   Rational();
+  Rational(long p, long q);
+  Rational(int p, int q);
+  Rational(unsigned p, unsigned q);
+
+  Rational(double);
+  Rational(float);
+
+  long numerator() const { return p; }
+  long denominator() const { return q; }
+
+  long to_l();
+  double to_d();
+  float to_f();
+  std::string to_s();
 
 private:
-  long long p;
-  long long q;
+  long p;
+  long q;
+  bool sign;
 };
 
 #endif
--- tests/test_rational.cxx
+++ tests/test_rational.cxx
@@ -5,6 +5,28 @@
 
 using std::string;
 
-TEST(RationalBasics, FirstTest) {
-  EXPECT_EQ(4,2);
+TEST(RationalBasics, Constructors) {
+  Rational r;
+
+  // null
+  r = Rational();
+
+  // p and q
+  long p = 2;
+  long q = 4;
+  r = Rational(p, q);
+
+  // decimal
+  double d = 1.3333333333333;
+  r = Rational(d);
+}
+
+TEST(RationalBasics, OutPut) {
+  Rational r;
+
+  r = Rational(1, 2);
+  EXPECT_EQ(r.to_d(), 1/2.0);
+  EXPECT_EQ(r.to_f(), (float)(1/2.0));
+  EXPECT_EQ(r.to_l(), 1/2);
+  EXPECT_EQ(r.to_s(), "1/2");
 }
