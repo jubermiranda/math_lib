3167df67e94d2e81840cf5573a9a12e015e787bd
uniapp <uniapp@uniapp>
2024-12-18 09:39:52 -0500

    initial rational number class

---
+++ include/rational.h
@@ -0,0 +1,13 @@
+#ifndef RATIONAL_H_
+#define RATIONAL_H_
+
+class Rational {
+public:
+  Rational();
+
+private:
+  long long p;
+  long long q;
+};
+
+#endif
---
+++ tests/test_rational.cxx
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "rational.h"
+#include "utils.h"
+
+using std::string;
+
+TEST(RationalBasics, FirstTest) {
+  EXPECT_EQ(4,2);
+}
