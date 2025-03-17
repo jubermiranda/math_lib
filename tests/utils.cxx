62b3259b0d2e463708f023e9f22e9a45345eef64
uniapp <uniapp@uniapp>
2025-03-17 15:31:30 -0400

    test some vectors dimentions

--- tests/utils.cxx
+++ tests/utils.cxx
@@ -2,20 +2,25 @@
 #include <string>
 #include <vector>
 
+
 using namespace std;
 
-unsigned t_utils::count_ocr(std::string str, std::string sub){
+namespace t_utils {
+
+unsigned count_ocr(std::string str, std::string sub) {
   unsigned result = 0;
   size_t pos = 0;
 
-  while((pos = str.find(sub, pos)) != string::npos){
+  while ((pos = str.find(sub, pos)) != string::npos) {
     result++;
     pos += sub.length();
   };
-  
+
   return result;
 }
 
-bool t_utils::is_sub_str(string str, string sub){
+bool is_sub_str(string str, string sub) {
   return (str.find(sub) != string::npos);
 }
+
+} // namespace t_utils
