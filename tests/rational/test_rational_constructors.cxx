1a766a98cfe14476febf3eeab792d592141c1965
uniapp <uniapp@uniapp>
2025-03-24 17:42:53 -0400

    test (p, q: long) (p != q). rm static_cast from tests

--- tests/rational/test_rational_constructors.cxx
+++ tests/rational/test_rational_constructors.cxx
@@ -16,14 +16,10 @@ TEST(RationalConstructors, Empty) {
 
   ASSERT_EQ(r.numerator(), 0);
   ASSERT_EQ(r.denominator(), 1);
-
-  ASSERT_EQ(static_cast<double>(r), 0.0);
-  ASSERT_EQ(static_cast<long>(r), 0);
 }
 
 // equals params p and q create a '1' Rational.
-// this impl should ever simplify, then:
-// ∀(p, q)((p, q ∈ long) and (p == q)) -> Rational(p, q): p = 1, q = 1
+// ∀(p, q)((p, q: long) and (p == q)) -> Rational(p, q): p = 1, q = 1
 TEST(RationalConstructors, IntegerEqualsParams) {
   Rational r;
   long p, q;
@@ -32,17 +28,39 @@ TEST(RationalConstructors, IntegerEqualsParams) {
   r = Rational(p, p);
   ASSERT_EQ(r.numerator(), 1);
   ASSERT_EQ(r.denominator(), 1);
-  ASSERT_EQ(static_cast<long>(r), 1);
 
   p = 3;
   r = Rational(p, p);
   ASSERT_EQ(r.numerator(), 1);
   ASSERT_EQ(r.denominator(), 1);
-  ASSERT_EQ(static_cast<long>(r), 1);
 
   p = -9;
   r = Rational(p, p);
   ASSERT_EQ(r.numerator(), 1);
   ASSERT_EQ(r.denominator(), 1);
-  ASSERT_EQ(static_cast<long>(r), 1);
+}
+
+// params p: long, q: long, represents rational p/q (q != 0)
+TEST(RationalConstructors, IntegerDiferentParams) {
+  Rational r;
+  long p, q;
+
+  p = 1;
+  q = 2;
+  r = Rational(p, q);
+  ASSERT_EQ(r.numerator(), 1);
+  ASSERT_EQ(r.denominator(), 2);
+
+  // ∀(p, q)(p == 0) -> q = 1
+  p = 0;
+
+  q = 4;
+  r = Rational(p, q);
+  ASSERT_EQ(r.numerator(), 0);
+  ASSERT_EQ(r.denominator(), 1);
+
+  q = 16;
+  r = Rational(p, q);
+  ASSERT_EQ(r.numerator(), 0);
+  ASSERT_EQ(r.denominator(), 1);
 }
