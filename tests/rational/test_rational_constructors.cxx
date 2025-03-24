ea5b9635a09c21dbe91316f9550acae6c5c81b2e
uniapp <uniapp@uniapp>
2025-03-24 17:33:44 -0400

    test p, q long and equals

--- tests/rational/test_rational_constructors.cxx
+++ tests/rational/test_rational_constructors.cxx
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
-#include "rational.h"
 #include "../utils.h"
+#include "rational.h"
 
 using std::string;
 
@@ -19,4 +19,30 @@ TEST(RationalConstructors, Empty) {
 
   ASSERT_EQ(static_cast<double>(r), 0.0);
   ASSERT_EQ(static_cast<long>(r), 0);
+}
+
+// equals params p and q create a '1' Rational.
+// this impl should ever simplify, then:
+// ∀(p, q)((p, q ∈ long) and (p == q)) -> Rational(p, q): p = 1, q = 1
+TEST(RationalConstructors, IntegerEqualsParams) {
+  Rational r;
+  long p, q;
+
+  p = 1;
+  r = Rational(p, p);
+  ASSERT_EQ(r.numerator(), 1);
+  ASSERT_EQ(r.denominator(), 1);
+  ASSERT_EQ(static_cast<long>(r), 1);
+
+  p = 3;
+  r = Rational(p, p);
+  ASSERT_EQ(r.numerator(), 1);
+  ASSERT_EQ(r.denominator(), 1);
+  ASSERT_EQ(static_cast<long>(r), 1);
+
+  p = -9;
+  r = Rational(p, p);
+  ASSERT_EQ(r.numerator(), 1);
+  ASSERT_EQ(r.denominator(), 1);
+  ASSERT_EQ(static_cast<long>(r), 1);
 }
